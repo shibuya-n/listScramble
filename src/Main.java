fd03a5fb41b8177bfb5f6a0012ce3a6b91c8c072
shibuya-n <109704872+shibuya-n@users.noreply.github.com>
2023-11-07 22:56:40 -0800

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,58 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner x = new Scanner(System.in);
+        System.out.println("How many names are there? ");
+        int input = x.nextInt();
+
+        String[] nameList = new String[input];
+        String[] scrambledStrings = new String[input];
+
+
+
+        for (int i = 0; i < nameList.length; i++) {
+            x = new Scanner(System.in);
+            System.out.println("What is #" + (i + 1) + " name? ");
+            String name = x.nextLine().strip();
+
+            nameList[i] = name;
+        }
+        System.out.println("Input words: " + Arrays.toString(nameList));
+
+        for (int i = 0; i < nameList.length; i++){
+            String temp =  nameList[i];
+            scrambledStrings[i] = scramble(temp);
+
+        }
+        System.out.println("Output words: " + Arrays.toString(scrambledStrings));
+
+
+
+    }
+    public static String scramble(String input){
+        String scramble = "";
+        String temp = "";
+
+
+        temp = input;
+
+        for (int i = 0; i < input.length(); i++){
+            int random = (int)(Math.random() * temp.length()-1) + 1;
+
+            if (temp.length() == 1) {
+                scramble += temp;
+                break;
+            }
+            else {
+                scramble += temp.charAt(random);
+
+                temp = temp.substring(0, random) + temp.substring(random + 1);
+            }
+
+        }
+
+        return scramble;
     }
 }
---
+++ src/Word.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Word {
+        String name;
+
+        public String result = "";
+        public Word(String x) {
+
+            name = x;
+
+
+
+        }
+
+        public String scramble(){
+            String scramble = "";
+            String temp = "";
+
+            scramble += name.substring(0,1);
+            temp += name.substring(1, name.length()-1);
+
+            for (int i = 0; i < name.length(); i++){
+                int random = (int)(Math.random() * temp.length()-1) + 1;
+
+                if (temp.length() == 1) {
+                    scramble += temp;
+                    break;
+                }
+                else {
+                    scramble += temp.charAt(random);
+
+                    temp = temp.substring(0, random) + temp.substring(random + 1);
+                }
+
+            }
+            scramble += name.substring(name.length()-1);
+            return scramble;
+        }
+
+        public static String name(){
+            System.out.println("First name and last name? ");
+            return getUserInput();
+
+        }
+        public static String getUserInput() {
+            Scanner input = new Scanner(System.in);
+            String response = input.nextLine();
+
+            return response;
+        }
+
+
+    }
+
